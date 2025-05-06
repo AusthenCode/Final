876ace7c7e5c5ca2484a7bc7c3aabf5d5a8af206
Austin <austinshelton546@hotmail.com>
2025-05-06 14:25:04 -0400

    new pets.js

--- routes/pets.js
+++ routes/pets.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router();
 const Pet = require('../models/pet');
 
+function checkAuthentication(req,res,next){
+    req.user={id:7}
+    if(!authenticated) res.status(501).json('FAILED TO AUTHENTICATE')
+    else next();
+}
 
 router.get('/', async (req, res) => {
     try {
@@ -12,7 +17,8 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', checkAuthentication, async (req, res) => {
+    console.log(req.user);
     try {
         const { name, breed, sex, age, about, image } = req.body;
         const newPet = new Pet({ name, breed, sex, age, about, image });
@@ -23,12 +29,27 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', checkAuthentication,async (req, res) => {
     try {
+        //find document
+        //check if document.authorID!=req.user.ID res.status(501).json('You are not authorized to delete this document')
         await Pet.findByIdAndDelete(req.params.id);
         res.json({ message: 'Pet deleted successfully!' });
     } catch (error) {
         res.status(500).send('Error deleting pet from database.');
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        await Pet.findByIdAndUpdate(req.params.id,
+        {$set: req.body},
+        {new: true} 
+    )
+        res.json({message: 'Pet updated successfully!'});
+    }
+    catch (error) {
+        res.status(500).send('Error: Shit i broke something.');
     }
 });
 
